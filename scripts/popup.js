5d5363917540418d88ff0b78d5f25ffdd5edbd13
LloydN01 <lloydna0527@gmail.com>
2024-03-01 01:24:24 +0800

    show summarised text in textarea

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,32 +2,27 @@ const apiUrl = "https://api.nlpcloud.io/v1/bart-large-cnn/summarization";
 const token = "";
 
 const summariseText = (textToSummarize) => {
-    const summarised_text = "";
-
-    fetch(apiUrl, {
+    return fetch(apiUrl, {
         method: "POST",
         headers: {
             Authorization: `Token ${token}`,
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            text: `${textToSummarize}`,
+            text: textToSummarize,
         }),
     })
         .then((response) => response.json())
-        .then((data) => {
-            summarised_text = data.summary_text;
-        })
+        .then((data) => data.summary_text)
         .catch((error) => {
             console.error("Error:", error);
+            return "Error when summarising text";
         });
-
-    return summarised_text
-}
+};
 
 const addText = (text) => {
-    const textbox = document.getElementById("summarised-text");
-    textbox.value = text;
+    const textBox = document.getElementById("text-box");
+    textBox.value = text;
 };
 
 document.getElementById("copy-btn").onclick = async () => {
@@ -39,15 +34,22 @@ document.getElementById("copy-btn").onclick = async () => {
             func: () => getSelection().toString(),
         });
     } catch (e) {
-        return; // ignoring an unsupported page like chrome://extensions
+        return;
     }
     addText(result);
     const summariseBtn = document.getElementById("summarise-btn");
     summariseBtn.style.visibility = "visible";
 };
 
-document.getElementById("summarise-btn").onclick = () => {
-    const textToSummarize = document.getElementById("summarised-text").value;
-    const summarised_text = summariseText(textToSummarize);
-    addText(summarised_text)
-}
+document.getElementById("summarise-btn").onclick = async () => {
+    const textBox = document.getElementById("text-box");
+    const loader = document.getElementById("loading-icon");
+    
+    textBox.style.display = "none";
+    loader.style.display = "block";
+    const summarised_text = await summariseText(textBox.value);
+    loader.style.display = "none"
+    textBox.style.display = "block"
+
+    addText(summarised_text);
+};
